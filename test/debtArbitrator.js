b75eb6e8ae63e6f9f31d40c79183e360904273ba
murisimov <murisimov@gmail.com>
2017-04-19 02:22:11 +0300

    Change throw tests to Cheating event tests, minor refactoring

--- test/debtArbitrator.js
+++ test/debtArbitrator.js
@@ -16,32 +16,17 @@ contract('DebtArbitrator', function(accounts) {
     .then(reverter.snapshot);
   });
 
-  it('should allow to repay', () => {
-    const borrower = accounts[3];
-    const value = 1000;
-    return Promise.resolve()
-    .then(() => debtArbitrator.borrow(value, {from: borrower}))
-    .then(() => debtArbitrator.repay(borrower, value, {from: OWNER}))
-    .then(() => debtArbitrator.debts(borrower))
-    .then(asserts.equal(0));
-  });
 
-  it('should fail on overflow when borrowing', () => {
-    const borrower = accounts[3];
-    const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
-    return Promise.resolve()
-    .then(() => debtArbitrator.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debtArbitrator.borrow(1, {from: borrower})));
-  });
+  // Borrowing
 
-  it('should fail on underflow when repaying', () => {
-    const borrower = accounts[3];
-    const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
-    return Promise.resolve()
-    .then(() => debtArbitrator.borrow(1, {from: borrower}))
-    .then(() => asserts.throws(debtArbitrator.repay(value, {from: borrower})));
+  it('should allow to borrow', () => {
+      const borrower = accounts[3];
+      const value = 1000;
+      return Promise.resolve()
+      .then(() => debtArbitrator.borrow(value, {from: borrower}))
+      .then(() => debtArbitrator.debts(borrower))
+      .then(asserts.equal(value));
   });
-
 
   it('should emit Borrowed event on borrow', () => {
     const borrower = accounts[3];
@@ -56,13 +41,38 @@ contract('DebtArbitrator', function(accounts) {
     });
   });
 
-  it('should allow to borrow', () => {
-      const borrower = accounts[3];
-      const value = 1000;
-      return Promise.resolve()
-      .then(() => debtArbitrator.borrow(value, {from: borrower}))
-      .then(() => debtArbitrator.debts(borrower))
-      .then(asserts.equal(value));
+  it('should emit Cheating event on overflow when borrowing', () => {
+    const borrower = accounts[3];
+    const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+    return Promise.resolve()
+    .then(() => debtArbitrator.borrow(value, {from: borrower}))
+    .then(() => debtArbitrator.borrow(1, {from: borrower}))
+    .then(result => {
+      assert.equal(result.logs.length, 1);
+      assert.equal(result.logs[0].event, 'Cheating');
+      assert.equal(result.logs[0].args.who, borrower);
+    });
+  });
+
+  it('should not allow owner to borrow', () => {
+    return Promise.resolve()
+    .then(() => debtArbitrator.borrow(1000, {from: OWNER}))
+    .then(result => assert.equal(result.logs.length, 0))
+    .then(() => debtArbitrator.debts(OWNER))
+    .then(asserts.equal(0));
+  });
+
+
+  // Repaying
+
+  it('should allow to repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debtArbitrator.borrow(value, {from: borrower}))
+    .then(() => debtArbitrator.repay(borrower, value, {from: OWNER}))
+    .then(() => debtArbitrator.debts(borrower))
+    .then(asserts.equal(0));
   });
 
   it('should emit Repaid event on repay', () => {
@@ -79,15 +89,28 @@ contract('DebtArbitrator', function(accounts) {
     });
   });
 
-  it('should not allow owner to borrow', () => {
-      return Promise.resolve()
-      .then(() => debtArbitrator.borrow(1000, {from: OWNER}))
-      .then(result => assert.equal(result.logs.length, 0))
-      .then(() => debtArbitrator.debts(OWNER))
-      .then(asserts.equal(0));
+  it('should not allow to repay more than the current debt', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debtArbitrator.borrow(value, {from: borrower}))
+    .then(() => debtArbitrator.repay(borrower, value + 500, {from: OWNER}))
+    .then(result => assert.equal(result.logs.length, 0))
+    .then(() => debtArbitrator.debts(borrower))
+    .then(asserts.equal(value));
   });
 
   it('should not allow not owner to repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debtArbitrator.borrow(value, {from: borrower}))
+    .then(() => debtArbitrator.repay(borrower, value, {from: borrower}))
+    .then(() => debtArbitrator.debts(borrower))
+    .then(asserts.equal(value));
+  });
+
+  it('should emit Cheating event when not owner is trying to repay', () => {
     const borrower = accounts[3];
     const value = 1000;
     return Promise.resolve()
@@ -97,9 +120,7 @@ contract('DebtArbitrator', function(accounts) {
       assert.equal(result.logs.length, 1);
       assert.equal(result.logs[0].event, 'Cheating');
       assert.equal(result.logs[0].args.who, borrower);
-    })
-    .then(() => debtArbitrator.debts(borrower))
-    .then(asserts.equal(value));
+    });
   });
 
   it('should direct you for inventing more tests');
