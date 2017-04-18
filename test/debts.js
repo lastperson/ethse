a1862e34a03e51d698410e18cdbc3da70ae9b1db
murisimov <murisimov@gmail.com>
2017-04-19 01:45:22 +0300

    Complete the missing tests

--- test/debts.js
+++ test/debts.js
@@ -34,6 +34,15 @@ contract('Debts', function(accounts) {
     .then(() => asserts.throws(debts.borrow(1, {from: borrower})));
   });
 
+  it('should fail on underflow when repaying', () => {
+    const borrower = accounts[3];
+    const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+    return Promise.resolve()
+    .then(() => debts.borrow(1, {from: borrower}))
+    .then(() => asserts.throws(debts.repay(value, {from: borrower})));
+  });
+
+
   it('should emit Borrowed event on borrow', () => {
     const borrower = accounts[3];
     const value = 1000;
@@ -47,13 +56,47 @@ contract('Debts', function(accounts) {
     });
   });
 
-  it('should allow to borrow');
+  it('should allow to borrow', () => {
+      const borrower = accounts[3];
+      const value = 1000;
+      return Promise.resolve()
+      .then(() => debts.borrow(value, {from: borrower}))
+      .then(() => debts.debts(borrower))
+      .then(asserts.equal(value));
+  });
 
-  it('should emit Repayed event on repay');
+  it('should emit Repayed event on repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.repay(borrower, value, {from: OWNER}))
+    .then(result => {
+      assert.equal(result.logs.length, 1);
+      assert.equal(result.logs[0].event, 'Repayed');
+      assert.equal(result.logs[0].args.by, borrower);
+      assert.equal(result.logs[0].args.value.valueOf(), value);
+    });
+  });
 
-  it('should not allow owner to borrow');
+  it('should not allow owner to borrow', () => {
+      return Promise.resolve()
+      .then(() => debts.borrow(1000, {from: OWNER}))
+      .then(result => assert.equal(result.logs.length, 0))
+      .then(() => debts.debts(OWNER))
+      .then(asserts.equal(0));
+  });
 
-  it('should not allow not owner to repay');
+  it('should not allow not owner to repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.repay(borrower, value, {from: borrower}))
+    .then(result => assert.equal(result.logs.length, 0))
+    .then(() => debts.debts(borrower))
+    .then(asserts.equal(value));
+  });
 
   it('should direct you for inventing more tests');
 });
