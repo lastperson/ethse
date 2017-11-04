d090d749db6dba458031b37cb877591911d6268a
Bogdan Kurinnyi <bogdan.kurinniy@ambisafe.co>
2017-11-04 18:11:59 +0200

    Add tests for Debts.sol

---
+++ contracts/Promiser.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.15;
+
+contract Promiser {
+    address owner;
+    int public balance;
+    mapping(address => int) public debts;
+    
+    event Borrow(address user, int amount);
+    event Refund(address user, int amount);
+    
+    function Promiser(int _balance) public {
+        owner = msg.sender;
+        balance = _balance;
+    }
+    
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+    
+    // check pottential balance change
+    modifier canBorrow(int amount) {
+        require(balance - amount >= 0 && owner != msg.sender);
+        _;
+    }
+
+    // check for oversend amount, we do not send short change
+    modifier canRefund(address borrower, int amount) {
+        require(debts[borrower] - amount >= 0 && borrower != msg.sender);
+        _;
+    }
+    
+    // send amount only positive
+    modifier isValidAmount(int amount) {
+        require(amount > 0);
+        _;
+    }
+    
+    // give debt
+    function borrow(int amount) public
+        isValidAmount(amount)
+        canBorrow(amount)
+    {
+        debts[msg.sender] += amount;
+        balance -= amount;
+        Borrow(msg.sender, amount);
+    }
+    
+    // return money back
+    function refund(address borrower, int amount) public
+        onlyOwner()
+        isValidAmount(amount)
+        canRefund(borrower, amount)
+    {
+        debts[borrower] -= amount;
+        balance += amount;
+        Refund(msg.sender, amount);
+    }
+}
--- test/debts.js
+++ test/debts.js
@@ -19,41 +19,99 @@ contract('Debts', function(accounts) {
   it('should allow to repay', () => {
     const borrower = accounts[3];
     const value = 1000;
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => debts.repay(borrower, value, {from: OWNER}))
-    .then(() => debts.debts(borrower))
-    .then(asserts.equal(0));
+
+    return (async function() {
+      await debts.borrow(value, {from: borrower});
+      await debts.repay(borrower, value, {from: OWNER});
+      let result = await debts.debts(borrower);
+      asserts.equal(result, 0);
+    }());
   });
 
   it('should fail on overflow when borrowing', () => {
     const borrower = accounts[3];
     const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debts.borrow(1, {from: borrower})));
+
+    return (async function() {
+      await debts.borrow(value, {from: borrower});
+      await asserts.throws(debts.borrow(1, {from: borrower}));
+    }());
   });
 
   it('should emit Borrowed event on borrow', () => {
     const borrower = accounts[3];
     const value = 1000;
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(result => {
+
+    return (async function() {
+      let result = await debts.borrow(value, {from: borrower});
       assert.equal(result.logs.length, 1);
       assert.equal(result.logs[0].event, 'Borrowed');
       assert.equal(result.logs[0].args.by, borrower);
       assert.equal(result.logs[0].args.value.valueOf(), value);
-    });
+    }());
   });
 
-  it('should allow to borrow');
+  it('should allow to borrow', () => {
+    const borrower = accounts[3];
+    const value = 1000;
 
-  it('should emit Repayed event on repay');
+    return (async function() {
+      await debts.borrow(value, {from: borrower});
+      let result = await debts.debts(borrower);
+      asserts.equal(result, 0);
+    }());
+  });
 
-  it('should not allow owner to borrow');
+  it('should emit Repayed event on repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
 
-  it('should not allow not owner to repay');
+    return (async function() {
+      await debts.borrow(value, {from: borrower});
+      let result = await debts.repay(borrower, value, {from: OWNER});
+      assert.equal(result.logs.length, 1);
+      assert.equal(result.logs[0].event, 'Repayed');
+      assert.equal(result.logs[0].args.by, borrower);
+      assert.equal(result.logs[0].args.value.valueOf(), value);
+    }());
+  });
 
-  it('should direct you for inventing more tests');
+  it('should not allow owner to borrow', () => {
+    const borrower = OWNER;
+    const value = 1000;
+
+    return (async function() {
+      let result;
+
+      result = await debts.borrow(value, {from: borrower});
+      asserts.equal(result, undefined);
+
+      result = await debts.debts(borrower);
+      asserts.equal(result, 0);
+    }());
+  });
+
+  it('should not allow not owner to repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+
+    return (async function() {
+      let result;
+
+      result = await debts.repay(borrower, value, {from: borrower});
+      asserts.equal(result, undefined);
+
+      result = await debts.debts(borrower);
+      asserts.equal(result, 0);
+    }());
+  });
+
+  it('should fail on underflow when repaying', () => {
+    const borrower = accounts[3];
+    const value = 1;
+
+    return (async function() {
+      await asserts.throws(debts.repay(borrower, value, {from: OWNER}));
+    }());
+  });
 });
