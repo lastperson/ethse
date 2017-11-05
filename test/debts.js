ed3c3bd9294d14106f405cd023c9d1fe293f673d
Unknown <pavel.mishin@gmail.com>
2017-11-05 18:41:00 +0200

    Tests
    Tests

--- test/debts.js
+++ test/debts.js
@@ -47,13 +47,59 @@ contract('Debts', function(accounts) {
     });
   });
 
-  it('should allow to borrow');
+  // =========== Pavlo Mishyn tests
 
-  it('should emit Repayed event on repay');
+  it('should allow to borrow', () => {
+    const borrower = accounts[4];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.debts(borrower))
+    .then(asserts.equal(1000));
+  });
 
-  it('should not allow owner to borrow');
+  it('should emit Repayed event on repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.repay(borrower, value, {from: OWNER}))
+    .then(result => {
+      assert.equal(result.logs.length, 1);
+      assert.equal(result.logs[0].event, 'Repayed');
+      assert.equal(result.logs[0].args.by, borrower);
+      assert.equal(result.logs[0].args.value.valueOf(), value);
+    });   
+  });
 
-  it('should not allow not owner to repay');
+  it('should not allow owner to borrow', () => {
+    const value = 1000;    
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: OWNER}))
+    .then(() => debts.debts(OWNER))
+    .then(() => asserts.equal(0))   
+  });
 
-  it('should direct you for inventing more tests');
+  it('should not allow not owner to repay', () => {
+    const borrower = accounts[3];
+    const randomGuy = accounts[4];
+    const value = 1000;
+
+    return Promise.resolve()
+      .then(() => debts.borrow(value, {from: borrower}))
+      .then(() => debts.repay(borrower, value, {from : randomGuy}))
+      .then(() => debts.debts(borrower))
+      .then(asserts.equal(value));
+  });
+
+  it('double borrow works correctly', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+
+    return Promise.resolve()
+      .then(() => debts.borrow(value, {from: borrower}))
+      .then(() => debts.borrow(value, {from: borrower}))
+      .then(() => debts.debts(borrower))
+      .then(asserts.equal(2 * value));    
+  });
 });
