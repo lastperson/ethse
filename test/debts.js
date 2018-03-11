f7d33c7e66b6afda437a4b60b8f93187ab5da626
Max Gor <maxweb4u@gmail.com>
2018-03-11 21:35:14 +0200

    added tests for Debts.sol contract

--- test/debts.js
+++ test/debts.js
@@ -47,13 +47,53 @@ contract('Debts', function(accounts) {
     });
   });
 
-  it('should allow to borrow');
+  it('should allow to borrow', () => {
+      const borrower = accounts[3];
+      const value = 1000;
+      return Promise.resolve()
+          .then(() => debts.borrow(value, {from: borrower}))
+          .then(() => debts.debts(borrower))
+          .then(result => {
+              assert.isTrue(value + result.toNumber() > result.toNumber());
+          });
+  });
 
-  it('should emit Repayed event on repay');
+  it('should emit Repayed event on repay', () => {
+      const borrower = accounts[3];
+      const value = 1000;
+      return Promise.resolve()
+          .then(() => debts.borrow(value, {from: borrower}))
+          .then(() => debts.repay(borrower, value, {from: OWNER}))
+          .then(result => {
+              assert.equal(result.logs.length, 1);
+              assert.equal(result.logs[0].event, 'Repayed');
+              assert.equal(result.logs[0].args.by, borrower);
+              assert.equal(result.logs[0].args.value.valueOf(), value);
+          });
+  });
 
-  it('should not allow owner to borrow');
+  it('should not allow owner to borrow', () => {
+      const borrower = accounts[3];
+      const value = 1000;
+      return Promise.resolve()
+          .then(() => debts.borrow(value, {from: borrower}))
+          .then(result => {
+              assert.equal(result.logs.length, 1);
+              assert.equal(result.logs[0].event, 'Borrowed');
+              assert.isFalse(result.logs[0].args.by == OWNER);
+          });
+  });
 
-  it('should not allow not owner to repay');
-
-  it('should direct you for inventing more tests');
+  it('should not allow not owner to repay', () => {
+      const borrower = accounts[3];
+      const value = 1000;
+      return Promise.resolve()
+          .then(() => debts.borrow(value, {from: borrower}))
+          .then(() => debts.repay(borrower, value, {from: OWNER}))
+          .then(result => {
+              assert.equal(result.logs.length, 1);
+              assert.equal(result.logs[0].event, 'Repayed');
+              assert.isFalse(result.logs[0].args.by == OWNER);
+          });
+  });
 });
