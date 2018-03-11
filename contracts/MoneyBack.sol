78e5a1be997cc040ff5923cb137cbbba235b7c71
Sergey Pilipchak <sergey.pilipchak@gmail.com>
2018-03-12 01:23:37 +0200

    updated the contract with new vision of it, prepared it for unit tests

--- contracts/MoneyBack.sol
+++ contracts/MoneyBack.sol
@@ -1,97 +1,88 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.15;
 
 contract MoneyBack{
+    address public owner;
+    mapping (address => uint) public debt;
+   
+    //event ErrorMessage(string);
+    //event Warning(string);
+    event Borrowed(address, uint);
+    event Payback(address, uint);
+    //event DebtIsPayedBack(string, address, string, address);
+    event DebtReview(address, uint);
+    //event DebtReviewError(string);
     
-     mapping (address => Debt) private creditor;
-     
-     struct Debt{
-         address debtor;
-         uint amount;
-     }
     
-    event ErrorMessage(string);
-    event Warning(string);
-    event BorrowedSuccesfully(address, string, uint, string, address);
-    event PaybackSuccessfull(address, string, uint, string, address );
-    event DebtIsPayedBack(string, address, string, address);
-    event DebtReviewSuccessfull(address, string, uint, string, address);
-    event DebtReviewError(string);
-    
-    function borrow(address toWhom, uint amount) public returns (bool, uint){
-        
-        if(msg.sender == toWhom){
-            ErrorMessage("you can't borrow yourself");
-            return (false, 0);
-        } else if(creditor[toWhom].amount > 0){
-            ErrorMessage("this person owns you, payback is required first");
-            return (false, 0);
-        }
-        
-        // borrower sends transaction
-        // with amount to borrow
-        // and the address whom to borrow to
-        creditor[toWhom].debtor = msg.sender;
-        creditor[toWhom].amount = amount;
-        BorrowedSuccesfully(msg.sender, 'barrowed', amount, 'from', toWhom);
-        return (true, amount);
+    modifier onlyOwner() {
+       require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyNotOwner() {
+        require(msg.sender != owner);
+        _;
     }
     
-    function payback(address fromWhom, uint amount) public returns (bool, uint){
+    function MoneyBack() {
+        owner = msg.sender;
+    }
+    
+    function borrow(uint _amount) onlyNotOwner public returns (bool){
+        // borrower sends transaction with amount to be borrowed
         
-        if (creditor[msg.sender].debtor != fromWhom){
-            ErrorMessage('Mentioned adress is not in your debtors list');
-            return (false, 0);
-        } else if (creditor[msg.sender].amount < amount) {
-            ErrorMessage('This debtor owns you less');
-            return (false, 0);
-        }
+        // not aloud:
+            // borrow 0 
+            // owner to borrow
+            
+        // aloud:
+            // multiple users can borrow
+            // borrow when not payed back full amount
         
-        // customer (borrower) sends transaction
-        // with address of debter (who pays back)
+        require(_amount != 0);
+            
+        debt[msg.sender] = _safeAdd(debt[msg.sender], _amount);
+        Borrowed(msg.sender, _amount);
+        return true;
+    }
+    
+    function payback(address _borrower, uint _amount) onlyOwner public returns (bool){
+        // owner sends transaction
+        // with address of debtor (who pays back)
         // and with amount to be payed back
-        creditor[msg.sender].amount -= amount;
-        PaybackSuccessfull(fromWhom, 'payed', amount, 'to', msg.sender );
         
-        // in case debtor payed not full amount back
-        if(creditor[msg.sender].amount != 0){
-            Warning('but he still needs to pay');
-            return (true, amount);
-        } 
+        // not aloud:
+            // payback 0 
+            // borrower to payback
+            // payback when nothing to be payed back
+            // pay bigger amount then borrowed
         
-        // if debtor payed full amount back
-        DebtIsPayedBack('Success!', fromWhom, 'owns nothing to', msg.sender);
-        return (true, amount);
+        require(_amount != 0);
+        require(debt[_borrower] != 0);
+        require(debt[_borrower] >= _amount);
+        
+        debt[_borrower] = _safeSub(debt[_borrower], _amount);
+        Payback(_borrower, _amount);
+        return true;
     }
     
-    function reviewDebt(address _debtor, address _creditor) public view returns (bool, uint) {
-        
-        // make sure only participants of a deal can review it
-        if (msg.sender != _debtor){
-            if (msg.sender != _creditor){
-                DebtReviewError('only deal participants can view it');
-                return (false, 0);
-            }
-            
-            // in case creditor never barrowed to mentioned debtor
-            // or debtor payed full amount back
-            if (creditor[_creditor].amount == 0){
-                DebtReviewSuccessfull(_debtor, 'owns ', 0, 'to ', _creditor);
-                return (true, 0);
-            } 
-            
-            DebtReviewSuccessfull(_debtor, 'owns ', creditor[_creditor].amount, 'to ', _creditor);
-            return (true, creditor[_creditor].amount);
-        }
-        
-        // in case creditor never barrowed to mentioned debtor
-        // or debtor payed full amount back
-        if (creditor[_creditor].amount == 0){
-                DebtReviewSuccessfull(_debtor, 'owns ', 0, 'to ', _creditor);
-                return (true, 0);
-            } 
-            
-        DebtReviewSuccessfull(_debtor, 'owns ', creditor[_creditor].amount, 'to ', _creditor);
-        return (true, creditor[_creditor].amount);
-       
+    function reviewDebt() onlyNotOwner public returns (uint) {
+       DebtReview(msg.sender, debt[msg.sender]);
+       return debt[msg.sender];
+    }
+    
+    function reviewDebtOwner(address _borrower) onlyOwner public returns(uint) {
+        DebtReview(_borrower, debt[_borrower]);
+        return debt[_borrower];
+    }
+    
+    function _safeSub(uint _a, uint _b) internal constant returns(uint) {
+        require(_b <= _a);
+        return _a - _b;
+    }
+
+    function _safeAdd(uint _a, uint _b) internal constant returns(uint) {
+        uint c = _a + _b;
+        require(c >= _a);
+        return c;
     }
 } 
