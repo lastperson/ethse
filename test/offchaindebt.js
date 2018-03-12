d6391ea982674025a3d88d0a6b532c7f3fa470a2
PriZ <priz_nak@mail.ru>
2018-03-12 10:37:32 +0200

    added semistandard style fix

--- test/offchaindebt.js
+++ test/offchaindebt.js
@@ -2,7 +2,7 @@ const Reverter = require('./helpers/reverter');
 const Asserts = require('./helpers/asserts');
 const OffChainDebts = artifacts.require('./OffChainDebts.sol');
 
-contract('OffChaitDebts', function(accounts) {
+contract('OffChaitDebts', function (accounts) {
   const reverter = new Reverter(web3);
   afterEach('revert', reverter.revert);
 
@@ -14,17 +14,17 @@ contract('OffChaitDebts', function(accounts) {
 
   before('setup', () => {
     return OffChainDebts.deployed()
-    .then(instance => debts = instance)
-    .then(reverter.snapshot);
+      .then(instance => (debts = instance))
+      .then(reverter.snapshot);
   });
 
-  //test borrow function
+  // test borrow function
   it('should allow to borrow', async () => {
-    await debts.borrow(AMOUNT, {from: BORROWER})
-    assert.equal((await debts.debts(BORROWER)).toNumber(), AMOUNT);
+    await debts.borrow(AMOUNT, {from: BORROWER});
+    assert.equal(await debts.debts(BORROWER), AMOUNT);
   });
 
-  it('should return TRUE on borrow', async() => {
+  it('should return TRUE on borrow', async () => {
     assert.isTrue(await debts.borrow.call(AMOUNT, {from: BORROWER}));
   });
 
@@ -32,13 +32,13 @@ contract('OffChaitDebts', function(accounts) {
     assert.isFalse(await debts.borrow.call(AMOUNT, {from: OWNER}));
   });
 
-   it('should fail on overflow when borrowing', async() => {
+  it('should fail on overflow when borrowing', async () => {
     const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
     await debts.borrow(value, {from: BORROWER});
     await asserts.throws(debts.borrow(1, {from: BORROWER}));
-  }); 
+  });
 
-  it('should emit Borrow event on borrow', async() => {
+  it('should emit Borrow event on borrow', async () => {
     var result = await debts.borrow(AMOUNT, {from: BORROWER});
     assert.equal(result.logs.length, 1);
     assert.equal(result.logs[0].event, 'Borrow');
@@ -46,14 +46,14 @@ contract('OffChaitDebts', function(accounts) {
     assert.equal(result.logs[0].args.amount.valueOf(), AMOUNT);
   });
 
-   //test repay function
-   it('should allow to repay', async () => {
+  // test repay function
+  it('should allow to repay', async () => {
     await debts.borrow(AMOUNT, {from: BORROWER});
     await debts.returnDebt(BORROWER, AMOUNT, {from: OWNER});
-    assert.equal(await debts.debts(BORROWER), 0)
-  }); 
+    assert.equal(await debts.debts(BORROWER), 0);
+  });
 
-  it('should emit ReturnDebt event on repay', async() => {
+  it('should emit ReturnDebt event on repay', async () => {
     await debts.borrow(AMOUNT, {from: BORROWER});
     var result = await debts.returnDebt(BORROWER, AMOUNT, {from: OWNER});
     assert.equal(result.logs.length, 1);
@@ -62,23 +62,23 @@ contract('OffChaitDebts', function(accounts) {
     assert.equal(result.logs[0].args.amount.valueOf(), AMOUNT);
   });
 
-  it('should not allow not owner to repay', async() => {
+  it('should not allow not owner to repay', async () => {
     await debts.borrow(AMOUNT, {from: BORROWER});
-    assert.isFalse(await debts.returnDebt.call(BORROWER, AMOUNT, {from: BORROWER}))
+    assert.isFalse(await debts.returnDebt.call(BORROWER, AMOUNT, {from: BORROWER}));
   });
 
-  it('should not repay more than debt amount', async() => {
+  it('should not repay more than debt amount', async () => {
     await debts.borrow(AMOUNT, {from: BORROWER});
     asserts.throws(debts.returnDebt(BORROWER, AMOUNT + 1, {from: OWNER}));
   });
 
-  it('should return TRUE on repay', async() => {
+  it('should return TRUE on repay', async () => {
     await debts.borrow(AMOUNT, {from: BORROWER});
     assert.isTrue(await debts.returnDebt.call(BORROWER, AMOUNT, {from: OWNER}));
   });
 
-  //constructor test
-  it('should set correct owner', async() => {
+  // constructor test
+  it('should set correct owner', async () => {
     asserts.equal(debts.owner.call(), OWNER);
   });
 });
