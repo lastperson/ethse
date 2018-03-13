84187357655ff61e471f083802ee8dbc09e6df13
Sergey Pilipchak <sergey.pilipchak@gmail.com>
2018-03-13 02:48:28 +0200

    added MoneyBack contract to deployment migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Debts = artifacts.require('./Debts.sol');
+const MoneyBack = artifacts.require('./MoneyBack.sol');
 
 module.exports = deployer => {
   deployer.deploy(Debts);
+  deployer.deploy(MoneyBack);
 };
