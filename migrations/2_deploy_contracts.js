ae5112f169ca3ced668acbcf0ece643e7465e684
danbogd <36646499+danbogd@users.noreply.github.com>
2018-03-18 16:27:04 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 var Loan = artifacts.require("Loan");
-var Debts = artifacts.require("Debts");
+var Debts = artifacts.require("MyOXO");
 module.exports = function(deployer) {
   
   deployer.deploy(Loan);
-  deployer.deploy(Debts);
+  deployer.deploy(myOXO);
 };
 
