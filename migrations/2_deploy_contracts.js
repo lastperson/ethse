183023ff9fa7da7784d8723fa0489a338deae1f9
yMk4 <35229372+yMk4@users.noreply.github.com>
2018-03-24 01:10:46 +0200

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const Debts = artifacts.require('./Debts.sol');
+const Credit = artifacts.require('./Credit.sol');
+const Ownable = artifacts.require('./Ownable.sol');
+const SafeMath = artifacts.require('./SafeMath.sol');
 
 module.exports = deployer => {
   deployer.deploy(Debts);
+  deployer.deploy(Credit);
+  deployer.deploy(Ownable);
+  deployer.deploy(SafeMath);
 };
