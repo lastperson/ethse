b36a32dec08e85dc15bf648971f08d4b8e8ff78e
Sergey Pilipchak <sergey.pilipchak@gmail.com>
2018-04-03 00:27:43 +0300

    added CryptOxo contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const Debts = artifacts.require('./Debts.sol');
 const MoneyBack = artifacts.require('./MoneyBack.sol');
+const CryptOxo = artifacts.require('./CryptOxo.sol');
 
 module.exports = deployer => {
   deployer.deploy(Debts);
   deployer.deploy(MoneyBack);
+  deployer.deploy(CryptOxo);
 };
